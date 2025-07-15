3f2b5497866ebdbc533ad95929ea0e5a15ad457a
Pascu-Victor <me@vpascu.ro>
2025-07-15 18:16:10 +0300

    add dummy syscall implementations

--- sysdeps/wos/generic/generic.cpp
+++ sysdeps/wos/generic/generic.cpp
@@ -25,6 +25,62 @@ void sys_exit(int status) {
 	__builtin_unreachable();
 }
 
+int sys_futex_wake(int *) {
+	// no futex support for now just panic
+	sys_libc_log("sys_futex_wake not supported");
+	sys_libc_panic();
+}
+int sys_futex_wait(int *, int, timespec const *) {
+	// no futex support for now just panic
+	sys_libc_log("sys_futex_wait not supported");
+	sys_libc_panic();
+}
+int sys_open(char const *, int, unsigned int, int *) {
+	// no open support for now just panic
+	sys_libc_log("sys_open not supported");
+	sys_libc_panic();
+}
+int sys_tcb_set(void *) {
+	// no TCB support for now just panic
+	sys_libc_log("sys_tcb_set not supported");
+	sys_libc_panic();
+}
+int sys_close(int) {
+	// no close support for now just panic
+	sys_libc_log("sys_close not supported");
+	sys_libc_panic();
+}
+int sys_clock_get(int, long *, long *) {
+	// no clock support for now just panic
+	sys_libc_log("sys_clock_get not supported");
+	sys_libc_panic();
+}
+int sys_anon_free(void *, unsigned long) {
+	// no anon free support for now just panic
+	sys_libc_log("sys_anon_free not supported");
+	sys_libc_panic();
+}
+int sys_seek(int, long, int, long *) {
+	// no seek support for now just panic
+	sys_libc_log("sys_seek not supported");
+	sys_libc_panic();
+}
+int sys_read(int, void *, unsigned long, long *) {
+	// no read support for now just panic
+	sys_libc_log("sys_read not supported");
+	sys_libc_panic();
+}
+int sys_vm_map(void *, unsigned long, int, int, int, long, void **) {
+	// no vm map support for now just panic
+	sys_libc_log("sys_vm_map not supported");
+	sys_libc_panic();
+}
+int sys_write(int, void const *, unsigned long, long *) {
+	// no write support for now just panic
+	sys_libc_log("sys_write not supported");
+	sys_libc_panic();
+}
+
 #ifndef MLIBC_BUILDING_RTLD
 
 [[noreturn]] void sys_thread_exit() {
